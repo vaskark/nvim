6dc6d367ce805846674d305ce960f55f4f40ae3f
vaskark <vaskark@gmail.com>
2024-02-14 18:58:58 -0500

    Added format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,10 +6,10 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 			},
-			-- format_on_save = {
-			-- 	timeout_ms = 500,
-			-- 	lsp_fallback = true,
-			-- },
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
 		})
 	end,
 }
