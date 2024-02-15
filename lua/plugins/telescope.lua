9e6ddbf0546565a094f928d07dcdf01e3e82407b
vaskark <vaskark@gmail.com>
2024-02-14 22:31:13 -0500

    Fixes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,39 +1,39 @@
 return {
 
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        pickers = {
-          find_files = {
-            find_command = {
-              "rg",
-              "--files",
-              "--smart-case",
-              "-g",
-              "!**/.cache/*",
-              "-g",
-              "!**/.git/*",
-            },
-          },
-        },
-      })
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						find_command = {
+							"rg",
+							"--files",
+							"--smart-case",
+							"-g",
+							"!**/.cache/*",
+							"-g",
+							"!**/.git/*",
+						},
+					},
+				},
+			})
+		end,
+	},
 
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown(),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
