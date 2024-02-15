ed5e9f466907926234ad5081e0877a4c33f2e938
vaskark <vaskark@gmail.com>
2024-02-14 20:22:52 -0500

    Fixes

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,10 +1,10 @@
 return {
 
-	"navarasu/onedark.nvim",
-	lazy = false,
-	config = function()
-		require("onedark").setup({
-			style = "light", -- dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-		})
-	end,
+  "navarasu/onedark.nvim",
+  lazy = false,
+  config = function()
+    require("onedark").setup({
+      style = "dark", -- dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    })
+  end,
 }
