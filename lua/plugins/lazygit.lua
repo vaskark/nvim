01ab09c71dc355444045fe71b76c01457bc5dc83
vaskark <vaskark@gmail.com>
2024-02-15 12:14:21 -0500

    Fixes

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
-	config = function()
-		require("telescope").load_extension("lazygit")
-	end,
+  config = function()
+    require("telescope").load_extension("lazygit")
+  end,
 }
