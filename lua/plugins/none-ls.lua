df2786cca1ae44e540b3332dd117c147f686be8a
vaskark <vaskark@gmail.com>
2024-02-14 19:04:05 -0500

    Added keymap description

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,6 +8,6 @@ return {
 				null_ls.builtins.formatting.stylua,
 			},
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format" })
 	end,
 }
