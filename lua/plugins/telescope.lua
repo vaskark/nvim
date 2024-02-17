ec02528be862c5236d31972e45511331ec621d24
vaskark <vaskark@gmail.com>
2024-02-17 11:22:03 -0500

    Moved keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
 
+  -- telescope
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
@@ -20,25 +21,30 @@ return {
 					},
 				},
 			})
+			vim.keymap.set("n", "<leader>fb", "<cmd> Telescope buffers theme=ivy <cr>", { desc = "List buffers" })
+			vim.keymap.set(
+				"n",
+				"<leader>fa",
+				"<cmd> Telescope find_files hidden=true <cr>",
+				{ desc = "Find all files" }
+			)
+			vim.keymap.set("n", "<leader>ff", "<cmd> Telescope find_files <cr>", { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fo", "<cmd> Telescope oldfiles <cr>", { desc = "Recent files" })
+			vim.keymap.set("n", "<leader>fs", "<cmd> Telescope grep_string <cr>", { desc = "String search" })
+			vim.keymap.set("n", "<leader>fw", "<cmd> Telescope live_grep <cr>", { desc = "Live text search" })
+			vim.keymap.set(
+				"n",
+				"<leader>fz",
+				"<cmd> Telescope current_buffer_fuzzy_find <cr>",
+				{ desc = "Current buffer text search" }
+			)
+
+			vim.keymap.set("n", "<leader>tb", "<cmd> Telescope <cr>", { desc = "Telescope" })
+			vim.keymap.set("n", "<leader>tc", "<cmd> Telescope colorscheme <cr>", { desc = "Colorschemes" })
 		end,
-
-		vim.keymap.set("n", "<leader>fb", "<cmd> Telescope buffers theme=ivy <cr>", { desc = "List buffers" }),
-		vim.keymap.set("n", "<leader>fa", "<cmd> Telescope find_files hidden=true <cr>", { desc = "Find all files" }),
-		vim.keymap.set("n", "<leader>ff", "<cmd> Telescope find_files <cr>", { desc = "Find files" }),
-		vim.keymap.set("n", "<leader>fo", "<cmd> Telescope oldfiles <cr>", { desc = "Recent files" }),
-		vim.keymap.set("n", "<leader>fs", "<cmd> Telescope grep_string <cr>", { desc = "String search" }),
-		vim.keymap.set("n", "<leader>fw", "<cmd> Telescope live_grep <cr>", { desc = "Live text search" }),
-		vim.keymap.set(
-			"n",
-			"<leader>fz",
-			"<cmd> Telescope current_buffer_fuzzy_find <cr>",
-			{ desc = "Current buffer text search" }
-		),
-
-		vim.keymap.set("n", "<leader>tb", "<cmd> Telescope <cr>", { desc = "Telescope" }),
-		vim.keymap.set("n", "<leader>tc", "<cmd> Telescope colorscheme <cr>", { desc = "Colorschemes" }),
 	},
 
+  -- telescope-ui-select
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
