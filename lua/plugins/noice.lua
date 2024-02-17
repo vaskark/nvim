79589c2a8a1acaf1cc97715c7023281db6cab6a9
vaskark <vaskark@gmail.com>
2024-02-17 11:21:14 -0500

    Moved keymaps

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,24 +1,23 @@
 return {
 
-  "folke/noice.nvim",
-  enabled = true,
-  event = "VeryLazy",
-  opts = {},
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("noice").setup({
-      cmdline = {
-        view = "cmdline",
-      },
-      messages = {
-        enabled = false,
-      },
-    })
-  end,
-
-  vim.keymap.set("n", "<leader>nd", "<cmd> NoiceDismiss <cr>", { desc = "Noice dismiss" }),
-  vim.keymap.set("n", "<leader>nh", "<cmd> NoiceHistory <cr>", { desc = "Noice history" }),
-  vim.keymap.set("n", "<leader>nt", "<cmd> NoiceTelescope <cr>", { desc = "Noice telescope" })
+	"folke/noice.nvim",
+	enabled = true,
+	event = "VeryLazy",
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("noice").setup({
+			cmdline = {
+				view = "cmdline",
+			},
+			messages = {
+				enabled = false,
+			},
+		})
+		vim.keymap.set("n", "<leader>nd", "<cmd> NoiceDismiss <cr>", { desc = "Noice dismiss" })
+		vim.keymap.set("n", "<leader>nh", "<cmd> NoiceHistory <cr>", { desc = "Noice history" })
+		vim.keymap.set("n", "<leader>nt", "<cmd> NoiceTelescope <cr>", { desc = "Noice telescope" })
+	end,
 }
