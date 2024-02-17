5a5a630a740b694a785a5dfc43966968214d017e
vaskark <vaskark@gmail.com>
2024-02-17 11:20:27 -0500

    Moved keymaps

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,13 +1,12 @@
 return {
 
-  "numToStr/Comment.nvim",
-  lazy = true,
-  config = function()
-    require("Comment").setup()
-  end,
-
-  vim.keymap.set("n", "<leader>/", function()
-    require("Comment.api").toggle.linewise.current()
-  end),
-  vim.keymap.set("v", "<leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>"),
+	"numToStr/Comment.nvim",
+	lazy = true,
+	config = function()
+		require("Comment").setup()
+	end,
+	vim.keymap.set("n", "<leader>/", function()
+		require("Comment.api").toggle.linewise.current()
+	end),
+	vim.keymap.set("v", "<leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>"),
 }
