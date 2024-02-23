7d4e4a2aa13c780144eb162a2b43f846e4bf4cfc
vaskark <vaskark@gmail.com>
2024-02-23 10:43:34 -0500

    Added auto-session plugin

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,18 @@
+return {
+
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      session_lens = {
+        buftypes_to_ignore = {},
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+      vim.keymap.set("n", "<leader>as", require("auto-session.session-lens").search_session, {
+        noremap = true,
+      }),
+    })
+  end,
+}
