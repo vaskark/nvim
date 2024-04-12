65118645b58217c89d8e1282de1292ee5ca067a9
vaskark <vaskark@gmail.com>
2024-04-11 22:19:41 -0400

    Added mellifluous theme

---
+++ lua/plugins/mellifluous.lua
@@ -0,0 +1,12 @@
+return {
+
+	"ramojus/mellifluous.nvim",
+	config = function()
+		require("mellifluous").setup({
+			color_set = "mellifluous", -- mellifluous, alduin, mountain, tender, kanagawa_dragon
+			transparent_background = {
+				enabled = true,
+			},
+		})
+	end,
+}
