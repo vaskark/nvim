d6cb5ee8a9239dda9998eedacfdc9c77447167ca
vaskark <vaskark@gmail.com>
2024-04-11 22:57:56 -0400

    Updates

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,7 +31,7 @@ return {
 
 			vim.keymap.set("n", "<S-k>", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 		end,
 	},
 }
