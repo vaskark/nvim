757fbcd61c8d1596391ad9f66d9fdfaf368e1887
vaskark <vaskark@gmail.com>
2024-04-11 22:19:58 -0400

    Changed code format keybind

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,6 +8,6 @@ return {
 				null_ls.builtins.formatting.stylua,
 			},
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format" })
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format" })
 	end,
 }
