c17ec951a2f6cfc8963b9692a0df58258c6a3d56
vaskark <vaskark@gmail.com>
2024-04-18 17:24:14 -0400

    Updates

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,10 +1,10 @@
 return {
 
-  "rose-pine/neovim",
-  name = "rose-pine",
-  opts = {
-    styles = {
-      transparency = true,
-    },
-  },
+	"rose-pine/neovim",
+	name = "rose-pine",
+	opts = {
+		styles = {
+			transparency = true,
+		},
+	},
 }
