df065d0d7af1bc788d9ed5e9fe4f75c4df634b61
vaskark <vaskark@gmail.com>
2024-04-18 20:44:49 -0400

    Updates

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,46 +1,23 @@
 -- default
-vim.cmd.colorscheme("bamboo")
+vim.cmd.colorscheme("lushwal")
 
--- lushwal config (should be moved elsewhere?)
+-- lushwal
 vim.g.lushwal_configuration = {
 	compile_to_vimscript = true,
 	color_overrides = nil,
 	addons = {
-		ale = false,
-		barbar = false,
-		bufferline_nvim = false,
-		coc_nvim = false,
-		dashboard_nvim = false,
-		fern_vim = false,
-		gina = false,
 		gitsigns_nvim = true,
-		hop_nvim = false,
-		hydra_nvim = false,
 		indent_blankline_nvim = true,
-		lightspeed_nvim = false,
-		lspsaga_nvim = false,
-		lsp_trouble_nvim = false,
 		lualine = true,
 		markdown = true,
-		mini_nvim = false,
 		native_lsp = true,
-		neogit = false,
-		neomake = false,
-		nerdtree = false,
 		nvim_cmp = true,
-		nvim_tree_lua = false,
-		nvim_ts_rainbow = false,
-		semshi = false,
 		telescope_nvim = true,
 		treesitter = true,
-		vim_dirvish = false,
-		vim_gitgutter = false,
-		vim_signify = false,
-		vim_sneak = false,
 		which_key_nvim = true,
 	},
 }
 
--- require("lushwal").add_reload_hook({
--- 	vim.cmd("LushwalCompile"),
--- })
+require("lushwal").add_reload_hook({
+	vim.cmd("LushwalCompile"),
+})
