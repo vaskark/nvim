75e8bdd97471a302fadc8066a31a3bb029b8891c
vaskark <vaskark@gmail.com>
2024-04-18 20:42:14 -0400

    Updates

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,3 +14,9 @@ map("n", "<leader>bd", "<cmd> bdelete <cr>", { desc = "Buffer delete" })
 
 -- clear search highlighting
 map("n", "<esc>", "<cmd> noh <return> <esc>")
+
+-- split navigation
+map('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
