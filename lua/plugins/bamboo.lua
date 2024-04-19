941fc11c660f9325c768de7be0eae4de83b9a33a
vaskark <vaskark@gmail.com>
2024-04-19 02:11:20 -0400

    Updates

--- lua/plugins/bamboo.lua
+++ lua/plugins/bamboo.lua
@@ -4,7 +4,7 @@ return {
 	priority = 1000,
   config = function()
     require('bamboo').setup({
-      toggle_style_key = "<leader>bs",
+      toggle_style_key = "nil",
 			lualine = {
 				transparent = true,
 			},
