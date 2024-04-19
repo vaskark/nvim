59b42067f843a198d94876dd1ba62565ffb6d7c4
vaskark <vaskark@gmail.com>
2024-04-19 00:43:06 -0400

    Updates

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,27 @@
 return {
 
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  opts = {
-    ensure_installed = { "lua", "bash", "regex", "vim", "vimdoc", "toml", "yaml", "json" },
-    auto_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },
-    autotag = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "bash", "regex", "vim", "vimdoc", "toml", "yaml", "json" },
+			auto_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			autotag = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
