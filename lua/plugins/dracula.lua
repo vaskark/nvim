b6faabf16da3ee56317d455dd0eb1e5cc202e0bb
vaskark <vaskark@gmail.com>
2024-04-19 03:42:56 -0400

    Updates

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -1,6 +1,6 @@
 return {
 	"Mofiqul/dracula.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {}
+  config = function()
+    require("dracula").setup()
+  end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,28 @@
 return {
-	-- telescope
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		},
-		opts = {
-			defaults = {
-				path_display = { "smart" },
-				mappings = {
-					i = {
-						["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
-						["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
-						["<C-q>"] = require("telescope.actions").send_selected_to_qflist + require("telescope.actions").open_qflist,
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					path_display = { "smart" },
+					mappings = {
+						i = {
+							["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+							["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+							["<C-q>"] = require("telescope.actions").send_selected_to_qflist
+								+ require("telescope.actions").open_qflist,
+						},
 					},
 				},
-			},
-		},
+			})
+			-- require("telescope").load_extension("fzf")
+		end,
 
 		vim.keymap.set("n", "<leader>fb", "<cmd> Telescope buffers theme=ivy <cr>", { desc = "List buffers" }),
 		vim.keymap.set("n", "<leader>fa", "<cmd> Telescope find_files hidden=true <cr>", { desc = "Find all files" }),
@@ -36,18 +40,17 @@ return {
 		vim.keymap.set("n", "<leader>tc", "<cmd> Telescope colorscheme <cr>", { desc = "Colorschemes" }),
 	},
 
-	-- telescope-ui-select
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown(),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+	-- {
+	-- 	"nvim-telescope/telescope-ui-select.nvim",
+	-- 	config = function()
+	-- 		require("telescope").setup({
+	-- 			extensions = {
+	-- 				["ui-select"] = {
+	-- 					require("telescope.themes").get_dropdown(),
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 		require("telescope").load_extension("ui-select")
+	-- 	end,
+	-- },
 }
