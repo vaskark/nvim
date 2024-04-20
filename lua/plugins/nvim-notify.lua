59567ca7a7dc629ab8a1658b254714868b5f93e5
vaskark <vaskark@gmail.com>
2024-04-20 17:17:03 -0400

    Updates

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -3,6 +3,7 @@ return {
 	opts = {
 		background_colour = "#000000",
 		render = "wrapped-compact", -- default, minimal, simple, compact, wrapped-compact
+		stages = "fade", -- fade_in_slide_out, fade, slide, static
 		timeout = 3000,
 	},
 }
