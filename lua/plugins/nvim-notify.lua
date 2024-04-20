5692b1dbd4b45dbb46ac88a120c577ba6815d46a
vaskark <vaskark@gmail.com>
2024-04-20 17:07:22 -0400

    Updates

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,6 +1,8 @@
 return {
-  "rcarriga/nvim-notify",
-  opts = {
-    background_colour = "#000000",
-  },
+	"rcarriga/nvim-notify",
+	opts = {
+		background_colour = "#000000",
+		render = "wrapped-compact", -- default, minimal, simple, compact, wrapped-compact
+		timeout = 3000,
+	},
 }
