4d40d92064211839ac2c079ed0210bf8ea46e929
vaskark <vaskark@gmail.com>
2024-04-20 23:24:33 -0400

    Updates

---
+++ lua/plugins/solarized.lua
@@ -0,0 +1,12 @@
+return {
+	"maxmx03/solarized.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("solarized").setup({
+			palette = "solarized", -- solarized, selenized
+			transparent = true,
+			-- theme = "neo", -- default, neo
+		})
+	end,
+}
