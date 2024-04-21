47ed273c41c34ecd8403a0fb7042dfd4ab5bd488
vaskark <vaskark@gmail.com>
2024-04-20 21:22:14 -0400

    Updates

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -1,6 +1,12 @@
 return {
 	"Mofiqul/dracula.nvim",
-  config = function()
-    require("dracula").setup()
-  end,
+	config = function()
+		require("dracula").setup({
+			colors = {},
+			italic_comment = false,
+			overrides = {},
+			show_end_of_buffer = false,
+			transparent_bg = true,
+		})
+	end,
 }
