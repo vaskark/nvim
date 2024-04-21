8b219f4918f3fcc488391054c5917b551567a3fe
vaskark <vaskark@gmail.com>
2024-04-21 06:18:49 -0400

    Updates

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -11,6 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- setup
 require("autocmds")
 require("options")
 require("keymaps")
