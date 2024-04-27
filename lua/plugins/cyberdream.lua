1212d73070cbc738029b30bd7dbb8d79de5d047a
unknown <vaskark@gmail.com>
2024-04-27 04:48:15 -0400

    Updates

---
+++ lua/plugins/cyberdream.lua
@@ -0,0 +1,13 @@
+return {
+	"scottmckendry/cyberdream.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("cyberdream").setup({
+			transparent = true,
+			hide_fillchars = true,
+			borderless_telescope = true,
+			terminal_colors = true,
+		})
+	end,
+}
