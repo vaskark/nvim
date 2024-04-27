ba454b36547c085bc4ff23318192e0f4ef1fc790
unknown <vaskark@gmail.com>
2024-04-27 04:56:02 -0400

    Updates

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -4,7 +4,7 @@ return {
 	priority = 1000,
 	config = function()
 		require("cyberdream").setup({
-			transparent = false,
+			transparent = true,
 			hide_fillchars = true,
 			borderless_telescope = false,
 		})
