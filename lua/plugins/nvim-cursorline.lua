aaecf7a0c7cfba33e73959e55ef8535b67edf4d9
vaskark <vaskark@gmail.com>
2024-04-28 19:10:07 -0400

    Updates

--- lua/plugins/nvim-cursorline.lua
+++ lua/plugins/nvim-cursorline.lua
@@ -1,18 +1,18 @@
 return {
-  "yamatsum/nvim-cursorline",
-  enabled = true,
-  config = function()
-    require("nvim-cursorline").setup({
-      cursorline = {
-        enable = true,
-        timeout = 500,
-        number = false,
-      },
-      cursorword = {
-        enable = true,
-        min_length = 3,
-        hl = { underline = true },
-      },
-    })
-  end,
+	"yamatsum/nvim-cursorline",
+	enabled = false,
+	config = function()
+		require("nvim-cursorline").setup({
+			cursorline = {
+				enable = true,
+				timeout = 500,
+				number = false,
+			},
+			cursorword = {
+				enable = true,
+				min_length = 3,
+				hl = { underline = true },
+			},
+		})
+	end,
 }
