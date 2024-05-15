9e5f8a29be08fe14695e37bf8cec3f7e85b884fa
vaskark <vaskark@gmail.com>
2024-05-15 17:31:37 -0400

    Updates

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,10 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	vim.keymap.set("n", "-", "<cmd> Oil <cr>", { desc = "Open parent directory" }),
 }
