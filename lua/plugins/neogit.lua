3678bbcf616ac558392475c03e3d3e56948c5aa5
vaskark <vaskark@gmail.com>
2024-05-19 18:34:47 -0400

    Updates

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,11 +1,11 @@
 return {
-  "NeogitOrg/neogit",
-  branch = "nightly",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
-  config = true,
-  vim.keymap.set("n", "<leader>ng", "<cmd> Neogit <cr>"),
+	"NeogitOrg/neogit",
+	branch = "master",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	config = true,
+	vim.keymap.set("n", "<leader>ng", "<cmd> Neogit <cr>"),
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -20,11 +20,11 @@ return {
 				["vim.lsp.util.stylize_markdown"] = true,
 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
-			signature = {
-				auto_open = {
-					enabled = false,
-				},
-			},
+			-- signature = {
+			-- 	auto_open = {
+			-- 		enabled = false,
+			-- 	},
+			-- },
 		},
 		presets = {
 			bottom_search = true, -- use a classic bottom cmdline for search
