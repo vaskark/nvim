3a07a3683696494e33b869f42f3d556df8fc8fb9
vaskark <vaskark@gmail.com>
2024-07-18 15:44:07 -0400

    Updates

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,9 +12,7 @@ return {
 		},
 	},
 	config = function()
-		local conform = require("conform")
-
-		conform.setup({
+		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 			},
