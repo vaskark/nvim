a267c133576f695f5a97a778c0ec26282a4d0172
vaskark <vaskark@gmail.com>
2024-07-18 15:45:39 -0400

    Updates

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -5,7 +5,6 @@ return {
 			auto_restore_enabled = false,
 			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
 		})
-
 		vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
 		vim.keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
 	end,
