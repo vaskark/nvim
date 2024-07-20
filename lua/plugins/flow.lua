dc832cd44339d310f935df6b66cd1f13529f7b93
vaskark <vaskark@gmail.com>
2024-07-20 15:36:17 -0400

    Updates

---
+++ lua/plugins/flow.lua
@@ -0,0 +1,14 @@
+return {
+	"0xstepit/flow.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		require("flow").setup_options({
+			transparent = true,
+			fluo_color = "orange", -- pink, yellow, orange, green
+			mode = "normal", -- normal, dark, bright
+			agressive_spell = "false",
+		})
+	end,
+}
