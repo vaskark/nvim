34fd2f2175c939cbbe5817532cceb1bf10adc62e
vaskark <vaskark@gmail.com>
2024-07-21 12:16:17 -0400

    Updates

--- lua/plugins/flow.lua
+++ lua/plugins/flow.lua
@@ -4,7 +4,7 @@ return {
 	priority = 1000,
 	opts = {},
 	config = function()
-		require("flow").setup_options({
+		require("flow").setup({
 			transparent = true,
 			fluo_color = "orange", -- pink, yellow, orange, green
 			mode = "normal", -- normal, dark, bright
