dd7515c6765974e158b5d0f60e539b4c9a76a69b
vaskark <vaskark@gmail.com>
2024-07-26 10:32:18 -0400

    Updates

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,5 @@
 return {
 	"tadmccorkle/markdown.nvim",
-	ft = "markdown", -- or 'event = "VeryLazy"'
-	opts = {
-		-- configuration here or empty for defaults
-	},
+	event = "VeryLazy",
+	opts = {},
 }
