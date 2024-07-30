5f0cf5e511d00e0b13c367e9902950d99fa4859b
vaskark <vaskark@gmail.com>
2024-07-30 15:02:01 -0400

    Updates

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,7 +1,20 @@
--- default
-vim.cmd("colorscheme rosebones")
+-- default colorscheme
+vim.g.SCHEME = "rosebones"
 
--- lushwal
--- require("lushwal").add_reload_hook({
--- 	vim.cmd("LushwalCompile"),
--- })
+-- lushwal (if needed)
+require("lushwal").add_reload_hook({
+	vim.cmd("LushwalCompile"),
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	nested = true,
+	callback = function()
+		pcall(vim.cmd.colorscheme, vim.g.SCHEME)
+	end,
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = function(params)
+		vim.g.SCHEME = params.match
+	end,
+})
