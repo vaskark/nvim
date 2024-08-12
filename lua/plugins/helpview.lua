f5a5a53032c4f6453c7a2e52077fd8db0151c38f
vaskark <vaskark@gmail.com>
2024-08-12 14:56:04 -0400

    Updates

--- lua/plugins/helpview.lua
+++ lua/plugins/helpview.lua
@@ -1,8 +1,7 @@
 return {
-	"OXY2DEV/helpview.nvim",
-	lazy = false,
-
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-	},
+    "OXY2DEV/helpview.nvim",
+    lazy = false,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
 }
