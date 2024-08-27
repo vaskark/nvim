1c4ae9b03384e3b95ce88537a83bee61011f0775
vaskark <vaskark@gmail.com>
2024-08-27 13:56:50 -0400

    Updates

--- lua/plugins/transparent.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"xiyaowong/transparent.nvim",
-	lazy = false,
-	config = function()
-		require("transparent").clear_prefix("NeoTree")
-		-- require("transparent").clear_prefix("lualine")
-	end,
-}
