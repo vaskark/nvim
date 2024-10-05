ec397643db987606eb0e1fc2a425ee47b9dd037f
vaskark <vaskark@gmail.com>
2024-10-05 13:52:47 -0400

    Updates

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -23,7 +23,11 @@ map("n", "<leader>bd", "<cmd> bdelete <cr>", { desc = "Buffer delete" })
 map("n", "<esc>", "<cmd> noh <return> <esc>")
 
 -- split navigation
-map('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
-map('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
-map('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
-map('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
+map("n", "<C-h>", "<C-w>h", { desc = "Move focus to the left window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move focus to the right window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move focus to the lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move focus to the upper window" })
+
+-- minty
+map("n", "<leader>mh", '<cmd> lua require("minty.huefy").open() <cr>', { desc = "Minty hues" })
+map("n", "<leader>ms", '<cmd> lua require("minty.shades").open() <cr>', { desc = "Minty shades" })
