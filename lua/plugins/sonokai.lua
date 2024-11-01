c5b08494b867d578045aa39f9b3f4b5b7460fd35
vaskark <vaskark@gmail.com>
2024-11-01 14:30:37 -0400

    Updates

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -1,3 +1,10 @@
 return {
 	"sainnhe/sonokai",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.sonokai_style = "andromeda" -- andromeda (default), atlantis, shusia, maia, espresso
+		vim.g.sonokai_better_performance = 1
+		vim.g.sonakai_transparent_background = 1
+	end,
 }
