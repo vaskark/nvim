d05134e645a37e04ee1addce11f22a01fe162939
vaskark <vaskark@gmail.com>
2024-11-01 14:00:00 -0400

    Updates

--- lua/plugins/solarized.lua
+++ lua/plugins/solarized.lua
@@ -2,13 +2,13 @@ return {
 	"maxmx03/solarized.nvim",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		require("solarized").setup({
-			transparent = {
-				enabled = true,
-			},
-			palette = "solarized", -- solarized (default), selenized
-			variant = "winter", -- "spring" | "summer" | "autumn" | "winter" (default)
-		})
+	---@type solarized.config
+	opts = {
+		transparent = { enabled = true },
+		palette = "solarized", -- solarized (default), selenized
+		variant = "spring", -- "spring" | "summer" | "autumn" | "winter" (default)
+	},
+	config = function(_, opts)
+		require("solarized").setup(opts)
 	end,
 }
