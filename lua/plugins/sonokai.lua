aff4e2c0607f428fdbd6f93c813be8bcaf956ed4
vaskark <vaskark@gmail.com>
2024-11-01 15:09:17 -0400

    Updates

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -3,7 +3,7 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.g.sonokai_style = "default" -- default, atlantis, andromeda, shusia, maia, espresso
+		vim.g.sonokai_style = "shusia" -- default, atlantis, andromeda, shusia, maia, espresso
 		vim.g.sonokai_better_performance = 1
 		vim.g.sonakai_transparent_background = 2
 	end,
