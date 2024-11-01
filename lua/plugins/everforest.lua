556b8b2d64529a3c6e6c606d1c2c2e761adbfe26
vaskark <vaskark@gmail.com>
2024-11-01 15:18:31 -0400

    Updates

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -3,7 +3,7 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.g.everforest_background = "medium" --hard, medium (default), soft
+		vim.g.everforest_background = "medium" -- hard, medium (default), soft
 		vim.g.everforest_better_performance = 1
 		vim.g.everforest_transparent_background = 2
 	end,
