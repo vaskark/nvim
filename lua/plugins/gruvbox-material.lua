49bb5fab1837a8126e3339f2753e01ab9977321a
vaskark <vaskark@gmail.com>
2024-11-01 15:02:51 -0400

    Updates

--- lua/plugins/gruvbox-material.lua
+++ lua/plugins/gruvbox-material.lua
@@ -1,3 +1,11 @@
 return {
 	"sainnhe/gruvbox-material",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.gruvbox_material_better_performance = 1
+		vim.g.gruvbox_material_transparent_background = 2
+		vim.g.gruvbox_material_background = "soft" -- hard, medium (default), soft
+		vim.g.gruvbox_material_foreground = "material" -- material (default), mix, original
+	end,
 }
