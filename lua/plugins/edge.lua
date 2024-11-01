a4e18e81f4db514de8125005a13d46e10dc35555
vaskark <vaskark@gmail.com>
2024-11-01 14:52:01 -0400

    Updates

---
+++ lua/plugins/edge.lua
@@ -0,0 +1,10 @@
+return {
+	"sainnhe/edge",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.edge_style = "default" -- default, aura, neon
+		vim.g.edge_better_performance = 1
+		vim.g.edge_transparent_background = 2
+	end,
+}
--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,3 +1,11 @@
 return {
 	"sainnhe/everforest",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.everforest_background = "medium" --hard, medium (default), soft
+		vim.g.everforest_better_performance = 1
+		vim.g.everforest_transparent_background = 2
+		vim.g.everforest_sign_column_background = "grey"
+	end,
 }
