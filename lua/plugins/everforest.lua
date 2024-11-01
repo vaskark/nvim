9ca41b5dd54a8a0a525fa9b273b04684181df1dd
vaskark <vaskark@gmail.com>
2024-11-01 14:53:29 -0400

    Updates

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -6,6 +6,5 @@ return {
 		vim.g.everforest_background = "medium" --hard, medium (default), soft
 		vim.g.everforest_better_performance = 1
 		vim.g.everforest_transparent_background = 2
-		vim.g.everforest_sign_column_background = "grey"
 	end,
 }
