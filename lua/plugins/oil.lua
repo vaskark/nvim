515360ed82cea16d4bf6793c0eec2da848ecab3f
vaskark <vaskark@gmail.com>
2024-11-01 13:21:46 -0400

    Updates

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,18 +2,19 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
-		default_file_explorer = true,
 		delete_to_trash = true,
+		win_options = {
+			wrap = true,
+		},
 		skip_confirm_for_simple_edits = true,
 		view_options = {
 			show_hidden = true,
-			natural_order = true,
 			is_always_hidden = function(name, _)
 				return name == " .. " or name == ".git"
 			end,
 		},
-		win_options = {
-			wrap = true,
+		float = {
+			padding = 20,
 		},
 	},
 	vim.keymap.set("n", "-", "<cmd> Oil <cr>", { desc = "Open parent directory" }),
