9b9e94511258c2287325405940d1299096ce0b68
vaskark <vaskark@gmail.com>
2024-11-02 16:58:49 -0400

    Updates

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,4 +4,12 @@ return {
 	opts = {
 		open_mapping = "<C-\\>",
 	},
+	vim.keymap.set("n", "<leader>th", "<cmd> ToggleTerm direction=horizontal <cr>", { desc = "Horizontal terminal" }),
+	vim.keymap.set(
+		"n",
+		"<leader>tv",
+		"<cmd> ToggleTerm direction=vertical size=50 <cr>",
+		{ desc = "Vertical terminal" }
+	),
+	vim.keymap.set("n", "<leader>tf", "<cmd> ToggleTerm direction=float <cr>", { desc = "Floating terminal" }),
 }
