cd115838b9ef2c0579d0f1b8954df5bafb9fe940
vaskark <vaskark@gmail.com>
2024-11-17 21:41:16 -0500

    Updates

--- lua/options.lua
+++ lua/options.lua
@@ -22,6 +22,9 @@ opt.smartcase = true
 opt.mouse = "a"
 opt.hlsearch = false
 
+opt.cursorline = true
+-- opt.cursorcolumn = true
+
 opt.number = true
 opt.relativenumber = true
 opt.ruler = false
--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,10 +1,9 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	---@module "ibl"
-	---@type ibl.config
-	opts = {},
 	config = function()
-		require("ibl").setup()
+		require("ibl").setup({
+			scope = { enabled = true },
+		})
 	end,
 }
