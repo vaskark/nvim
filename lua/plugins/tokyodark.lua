280c07f3b15b1351c998f63146cd0526b4810abe
vaskark <vaskark@gmail.com>
2024-12-04 09:51:04 -0500

    Updates

--- lua/plugins/tokyodark.lua
+++ lua/plugins/tokyodark.lua
@@ -4,11 +4,11 @@ return {
 		transparent_background = true,
 		gamma = 1.00, -- adjust the brightness of the theme
 		styles = {
-			comments = { italic = false }, -- style for comments
-			keywords = { italic = false }, -- style for keywords
-			identifiers = { italic = false }, -- style for identifiers
-			functions = {}, -- style for functions
-			variables = {}, -- style for variables
+			comments = { italic = false },
+			keywords = { italic = false },
+			identifiers = { italic = false },
+			functions = {},
+			variables = {},
 		},
 	},
 }
