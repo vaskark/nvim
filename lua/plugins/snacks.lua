828793250820de277e72f8ddaabe74831b1ff0fd
vaskark <vaskark@gmail.com>
2024-12-06 18:12:46 -0500

    Updates

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -140,24 +140,24 @@ return {
 			desc = "Prev Reference",
 			mode = { "n", "t" },
 		},
-		-- {
-		-- 	"<leader>N",
-		-- 	desc = "Neovim News",
-		-- 	function()
-		-- 		Snacks.win({
-		-- 			file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-		-- 			width = 0.6,
-		-- 			height = 0.6,
-		-- 			wo = {
-		-- 				spell = false,
-		-- 				wrap = false,
-		-- 				signcolumn = "yes",
-		-- 				statuscolumn = " ",
-		-- 				conceallevel = 3,
-		-- 			},
-		-- 		})
-		-- 	end,
-		-- },
+		{
+			"<leader>N",
+			desc = "Neovim News",
+			function()
+				Snacks.win({
+					file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+					width = 0.6,
+					height = 0.6,
+					wo = {
+						spell = false,
+						wrap = false,
+						signcolumn = "yes",
+						statuscolumn = " ",
+						conceallevel = 3,
+					},
+				})
+			end,
+		},
 		{
 			"<leader>db",
 			function()
