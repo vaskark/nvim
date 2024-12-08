82c679f6c86c0ef6ad726d3c31785709384673b7
vaskark <vaskark@gmail.com>
2024-12-08 17:00:19 -0500

    Updates

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- lazy.nvim
+-- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
