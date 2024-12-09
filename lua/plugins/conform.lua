b38fb76ece54268a2cb1c822323555d1903f4f06
vaskark <vaskark@gmail.com>
2024-12-09 12:26:15 -0500

    Updates

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,27 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	keys = {
-		{
-			"<leader>cf",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "n",
-			desc = "Format buffer",
-		},
-	},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				sh = { "shfmt" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			},
-		})
-	end,
+  "stevearc/conform.nvim",
+  enabled = false,
+  event = { "BufReadPre", "BufNewFile" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "n",
+      desc = "Format buffer",
+    },
+  },
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        sh = { "shfmt" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
+    })
+  end,
 }
