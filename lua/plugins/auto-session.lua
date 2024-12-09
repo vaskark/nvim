b2083d11e2f8e2cb1700367058d87ecc4010aa57
vaskark <vaskark@gmail.com>
2024-12-09 13:06:00 -0500

    Updates

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,15 +1,14 @@
 return {
-	"rmagatti/auto-session",
+  "rmagatti/auto-session",
   lazy = false,
   ---@module "auto-session"
   ---@type AutoSession.Config
-	opts = {			
-		auto_restore = false,
-		suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
-	},	
-  keys = {
-    { '<leader>wr', '<cmd>SessionRestore<CR>', desc = 'Restore session' },
-    { '<leader>ws', '<cmd>SessionSave<CR>', desc = 'Save session' },
+  opts = {
+    auto_restore = false,
+    suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
   },
-
+  keys = {
+    { "<leader>sr", "<cmd>SessionRestore<CR>", desc = "Restore session" },
+    { "<leader>ss", "<cmd>SessionSave<CR>", desc = "Save session" },
+  },
 }
