b1eab8fbf3d3b664917fe70199e15d7d598ee81f
vaskark <vaskark@gmail.com>
2024-12-08 22:58:47 -0500

    Updates

---
+++ lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,10 @@
+return {
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "VeryLazy",
+  priority = 1000,
+  config = function()
+    require("tiny-inline-diagnostic").setup({
+      preset = "modern", -- modern, classic, minimal, powerline, ghost, simple
+    })
+  end,
+}
