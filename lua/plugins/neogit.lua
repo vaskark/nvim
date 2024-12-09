0182aee20c0eb5f728a746e6b04b9fdc7fa024f1
vaskark <vaskark@gmail.com>
2024-12-09 14:34:31 -0500

    Updates

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,21 +1,10 @@
 return {
-	"NeogitOrg/neogit",
-	branch = "master",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"sindrets/diffview.nvim",
-		"nvim-telescope/telescope.nvim",
-	},
-	config = function()
-		require("neogit").setup({
-			graph_style = "unicode",
-			highlight = {
-				italic = false,
-				bold = false,
-				underline = false,
-			},
-			auto_close_console = true,
-		})
-		vim.keymap.set("n", "<leader>ng", "<cmd> Neogit <cr>")
-	end,
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = true,
+  vim.keymap.set("n", "<leader>ng", "<cmd> Neogit <cr>"),
 }
