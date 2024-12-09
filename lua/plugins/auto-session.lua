33915cf4d6cba1f73645cdbd3b14fb4e80d21da4
vaskark <vaskark@gmail.com>
2024-12-09 13:06:42 -0500

    Updates

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -5,7 +5,7 @@ return {
   ---@type AutoSession.Config
   opts = {
     auto_restore = false,
-    suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+    suppressed_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
   },
   keys = {
     { "<leader>sr", "<cmd>SessionRestore<CR>", desc = "Restore session" },
