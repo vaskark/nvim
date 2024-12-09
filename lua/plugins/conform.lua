cfc4772a49c4ad530766c20e7bebebd1468e24a1
vaskark <vaskark@gmail.com>
2024-12-09 14:29:46 -0500

    Updates

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -30,8 +30,4 @@ return {
       },
     },
   },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
 }
