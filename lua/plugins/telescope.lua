41a74f172faee1de5d6fd3acd969fc14e11d9fd0
vaskark <vaskark@gmail.com>
2024-12-08 20:14:43 -0500

    Updates

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,7 +38,7 @@ return {
 		vim.keymap.set("n", "<leader>fa", "<cmd> Telescope find_files hidden=true <cr>", { desc = "Find all files" })
 		vim.keymap.set("n", "<leader>fb", "<cmd> Telescope buffers theme=ivy <cr>", { desc = "List buffers" })
 		vim.keymap.set("n", "<leader>ff", "<cmd> Telescope find_files <cr>", { desc = "Find files" })
-		vim.keymap.set("n", "<leader>fo", "<cmd> Telescope oldfiles <cr>", { desc = "Recent files" })
+		vim.keymap.set("n", "<leader>fr", "<cmd> Telescope oldfiles <cr>", { desc = "Recent files" })
 		vim.keymap.set("n", "<leader>fs", "<cmd> Telescope grep_string <cr>", { desc = "String search" })
 		vim.keymap.set("n", "<leader>fw", "<cmd> Telescope live_grep <cr>", { desc = "Live text search" })
 		vim.keymap.set(
