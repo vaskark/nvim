cff2905fd2c94080fb245335523549385f77a021
vaskark <vaskark@gmail.com>
2024-12-10 17:39:46 -0500

    Updates

--- lua/plugins/melange.lua
+++ lua/plugins/melange.lua
@@ -1,3 +1,13 @@
 return {
-	"savq/melange-nvim",
+  "savq/melange-nvim",
+  config = function()
+    local group = vim.api.nvim_create_augroup("OverrideMelange", {})
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "melange",
+      callback = function()
+        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+      end,
+      group = group,
+    })
+  end,
 }
