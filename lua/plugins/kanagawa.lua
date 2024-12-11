8aa2c5f138a6176f37c56fedfcdfe202282cd9cb
vaskark <vaskark@gmail.com>
2024-12-11 00:18:14 -0500

    Updates

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,16 +2,7 @@ return {
   "rebelot/kanagawa.nvim",
   config = function()
     require("kanagawa").setup({
-      compile = false,
-      undercurl = true,
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
       transparent = true,
-      dimInactive = false,
-      terminalColors = true,
       colors = {
         theme = {
           all = {
