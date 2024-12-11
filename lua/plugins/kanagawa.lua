a847c2b77ca0ddc7656d90452e4152abf36a14b3
vaskark <vaskark@gmail.com>
2024-12-10 19:43:31 -0500

    Updates

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -3,6 +3,8 @@ return {
   config = function()
     require("kanagawa").setup({
       transparent = true,
+
+      -- remove gutter background
       colors = {
         theme = {
           all = {
@@ -12,9 +14,12 @@ return {
           },
         },
       },
+
+      -- modern windows
       overrides = function(colors)
         local theme = colors.theme
         return {
+          -- modern telescope ui (borderless)
           TelescopeTitle = { fg = theme.ui.special, bold = true },
           TelescopePromptNormal = { bg = theme.ui.bg_p1 },
           TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
@@ -22,8 +27,23 @@ return {
           TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
           TelescopePreviewNormal = { bg = theme.ui.bg_dim },
           TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+          -- transparent floating windows
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          -- popup menus
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
         }
       end,
+
       theme = "dragon",
       background = {
         dark = "dragon",
