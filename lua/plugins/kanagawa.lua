5b9843f8222b9db9ba659cdaabe35fda2704c00d
vaskark <vaskark@gmail.com>
2024-12-10 19:50:19 -0500

    Updates

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -15,18 +15,17 @@ return {
         },
       },
 
-      -- modern windows
       overrides = function(colors)
         local theme = colors.theme
         return {
           -- modern telescope ui (borderless)
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          -- TelescopeTitle = { fg = theme.ui.special, bold = true },
+          -- TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          -- TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          -- TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          -- TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          -- TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          -- TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
 
           -- transparent floating windows
           NormalFloat = { bg = "none" },
