b9ac70a165974a568eb6882b9dab68eab2efd1bb
vaskark <vaskark@gmail.com>
2024-12-10 19:55:43 -0500

    Updates

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,7 +2,16 @@ return {
   "rebelot/kanagawa.nvim",
   config = function()
     require("kanagawa").setup({
-      transparent = true,
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = true, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
 
       -- remove gutter background
       colors = {
