7e7f47d0b7f835d8eaf93be94b63f80daacd7606
vaskark <vaskark@gmail.com>
2024-12-10 20:29:56 -0500

    Updates

--- lua/plugins/zenbones.lua
+++ lua/plugins/zenbones.lua
@@ -4,8 +4,16 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.zenbones = {
-      transparent_background = true,
-    }
+    vim.g.duckbones_transparent_background = true
+    vim.g.forestbones_transparent_background = true
+    vim.g.kanagawabones_transparent_background = true
+    vim.g.neobones_transparent_background = true
+    vim.g.nordbones_transparent_background = true
+    vim.g.randombones_transparent_background = true
+    vim.g.rosebones_transparent_background = true
+    vim.g.seoulbones_transparent_background = true
+    vim.g.tokyobones_transparent_background = true
+    vim.g.vimbones_transparent_background = true
+    vim.g.zenbones_transparent_background = true
   end,
 }
