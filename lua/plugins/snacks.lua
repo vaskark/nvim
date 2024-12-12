38c868a9a4968edcdab0057d5f3265aa277cd9ac
vaskark <vaskark@gmail.com>
2024-12-11 22:05:16 -0500

    Updates

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -32,10 +32,31 @@ return {
     notifier = {
       enabled = true,
       timeout = 3000,
+
+      -- simple LSP progress
+      vim.api.nvim_create_autocmd("LspProgress", {
+        ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+        callback = function(ev)
+          local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+          vim.notify(vim.lsp.status(), "info", {
+            id = "lsp_progress",
+            title = "LSP Progress",
+            opts = function(notif)
+              notif.icon = ev.data.params.value.kind == "end" and " "
+                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+          })
+        end,
+      }),
     },
     quickfile = { enabled = true },
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
+    statuscolumn = {
+      enabled = true,
+      folds = {
+        open = false,
+      },
+    },
     words = {
       enabled = true,
     },
@@ -137,6 +158,20 @@ return {
       end,
       desc = "Lazygit Log (cwd)",
     },
+    -- {
+    --   "<c-/>",
+    --   function()
+    --     Snacks.terminal()
+    --   end,
+    --   desc = "Toggle Terminal",
+    -- },
+    -- {
+    --   "<c-_>",
+    --   function()
+    --     Snacks.terminal()
+    --   end,
+    --   desc = "which_key_ignore",
+    -- },
     {
       "]]",
       function()
