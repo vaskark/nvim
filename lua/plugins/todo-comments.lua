ab574d6ebdf04ae291e5fd2107b0dd58ddfd3482
vaskark <vaskark@gmail.com>
2024-12-12 12:33:21 -0500

    Updates

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,5 +3,8 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
     signs = false,
+    highlight = {
+      pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+    },
   },
 }
