90af54e5e30ef5045f708a819627110089f514a6
vaskark <vaskark@gmail.com>
2024-12-12 12:02:41 -0500

    Updates

--- lua/plugins/tokyodark.lua
+++ lua/plugins/tokyodark.lua
@@ -2,13 +2,13 @@ return {
 	"tiagovla/tokyodark.nvim",
 	opts = {
 		transparent_background = true,
-		gamma = 1.00, -- adjust the brightness of the theme
-		styles = {
-			comments = { italic = false },
-			keywords = { italic = false },
-			identifiers = { italic = false },
-			functions = {},
-			variables = {},
-		},
+		-- gamma = 1.00, -- adjust the brightness of the theme
+		-- styles = {
+		-- 	comments = { italic = false },
+		-- 	keywords = { italic = false },
+		-- 	identifiers = { italic = false },
+		-- 	functions = {},
+		-- 	variables = {},
+		-- },
 	},
 }
