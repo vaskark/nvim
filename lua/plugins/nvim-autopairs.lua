340c5c5e3b03f7ad0d48cefeb6fe4496b2b751fc
vaskark <vaskark@gmail.com>
2024-12-13 15:23:23 -0500

    Updates

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -9,9 +9,7 @@ return {
     autopairs.setup({
       check_ts = true, -- enable treesitter
       ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java
+        lua = { "string" },
       },
     })
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
