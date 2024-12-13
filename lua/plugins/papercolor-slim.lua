4ed97038a0f7cc3fea3e496ebf5686ae83ae4d76
vaskark <vaskark@gmail.com>
2024-12-13 17:07:44 -0500

    Updates

--- lua/plugins/papercolor-slim.lua
+++ lua/plugins/papercolor-slim.lua
@@ -2,10 +2,10 @@ return {
   "pappasam/papercolor-theme-slim",
   config = function()
     vim.cmd([[
-      augroup custom_papercolorslim_transparent_background
-        autocmd!
-        autocmd ColorScheme PaperColorSlim highlight Normal guibg=NONE
-      augroup end
-  ]])
+    augroup custom_papercolorslim_transparent_background
+      autocmd!
+      autocmd ColorScheme PaperColorSlim highlight Normal guibg=NONE
+    augroup end
+    ]])
   end,
 }
