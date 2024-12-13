ba9a9978422af639a5c8b92f1b7afa2cf2072d21
vaskark <vaskark@gmail.com>
2024-12-13 16:46:08 -0500

    Updates

--- lua/plugins/papercolor.lua
+++ lua/plugins/papercolor.lua
@@ -1,3 +1,12 @@
 return {
   "NLKNguyen/papercolor-theme",
+  -- config = function()
+  --   vim.g.PaperColor_Theme_Options = {
+  --     'theme': {
+  --       'default': {
+  --         'transparent_background': 0
+  --       }
+  --     }
+  --   }
+  -- end,
 }
