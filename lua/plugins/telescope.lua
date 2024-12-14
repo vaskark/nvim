80c2dc533bc2fc51e1b2f03d9797152a63a1b5d0
vaskark <vaskark@gmail.com>
2024-12-13 21:52:30 -0500

    Updates

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,11 +19,11 @@ return {
           },
         },
       },
-      -- pickers = {
-      --   find_files = {
-      --     theme = "dropdown",
-      --   },
-      -- },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--no-config", "--files", "--sortr=modified" },
+        },
+      },
       extensions = {
         fzf = {},
         ["ui-select"] = {
