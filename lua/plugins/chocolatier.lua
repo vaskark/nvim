967657aac30d509ba40c11637a45d4c3c97466fd
vaskark <vaskark@gmail.com>
2024-12-13 20:40:27 -0500

    Updates

--- lua/plugins/chocolatier.lua
+++ lua/plugins/chocolatier.lua
@@ -1,8 +1,13 @@
 return {
-	"qaptoR-nvim/chocolatier.nvim",
-	priority = 1000,
-	config = true,
-	opts = {
-		transparent_mode = true,
-	},
+  "qaptoR-nvim/chocolatier.nvim",
+  priority = 1000,
+  config = true,
+  opts = {
+    transparent_mode = true,
+    overrides = {
+      CursorLineNr = {
+        bg = "none",
+      },
+    },
+  },
 }
