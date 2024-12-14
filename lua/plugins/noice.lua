a791251ba2a0f939485248de5f40a851deeb0a26
vaskark <vaskark@gmail.com>
2024-12-14 12:45:57 -0500

    Updates

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,24 +1,24 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
-	opts = {
-		cmdline = {
-			view = "cmdline", -- cmdline, cmdline_popup
-		},
-		notify = { enabled = false },
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-			},
-		},
-		-- vim.keymap.set("n", "<leader>nd", "<cmd> NoiceDismiss <cr>", { desc = "Noice dismiss" }),
-		-- vim.keymap.set("n", "<leader>nh", "<cmd> NoiceHistory <cr>", { desc = "Noice history" }),
-		-- vim.keymap.set("n", "<leader>nt", "<cmd> NoiceTelescope <cr>", { desc = "Noice telescope" }),
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    cmdline = {
+      view = "cmdline", -- cmdline, cmdline_popup
+    },
+    notify = { enabled = false },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    -- vim.keymap.set("n", "<leader>nd", "<cmd> NoiceDismiss <cr>", { desc = "Noice dismiss" }),
+    -- vim.keymap.set("n", "<leader>nh", "<cmd> NoiceHistory <cr>", { desc = "Noice history" }),
+    -- vim.keymap.set("n", "<leader>nt", "<cmd> NoiceTelescope <cr>", { desc = "Noice telescope" }),
+  },
 }
