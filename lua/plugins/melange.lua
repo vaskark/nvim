5046938456371f35a14762256d484c34af7222e7
vaskark <vaskark@gmail.com>
2024-12-15 13:35:22 -0500

    Updates

--- lua/plugins/melange.lua
+++ lua/plugins/melange.lua
@@ -5,7 +5,8 @@ return {
     vim.api.nvim_create_autocmd("ColorScheme", {
       pattern = "melange",
       callback = function()
-        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
       end,
       group = group,
     })
