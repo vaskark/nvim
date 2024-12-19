9f42b9587e30dedf7239b38fe6de66ef22c7d999
vaskark <vaskark@gmail.com>
2024-12-19 11:41:54 -0500

    Updates

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -2,8 +2,6 @@ return {
   "saghen/blink.cmp",
   dependencies = "rafamadriz/friendly-snippets",
   version = "v0.*",
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
   opts = {
     keymap = { preset = "default" },
     appearance = {
