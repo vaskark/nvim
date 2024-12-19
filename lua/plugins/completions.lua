7d838591eec94f2f21785ba4fb05de04b8950023
vaskark <vaskark@gmail.com>
2024-12-19 11:43:07 -0500

    Updates

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -8,10 +8,6 @@ return {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
     },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-      -- cmdline = {},
-    },
     signature = { enabled = true },
   },
   opts_extend = { "sources.default" },
