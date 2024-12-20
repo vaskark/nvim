63d7b77382ede9b7adc489013ac55bf4864ed39c
vaskark <vaskark@gmail.com>
2024-12-20 11:32:01 -0500

    Updates

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,24 +1,24 @@
 return {
-	"nvim-neorg/neorg",
-	lazy = false,
-	version = "*",
-	config = true,
-	-- require("neorg").setup({
-	-- 	load = {
-	-- 		["core.defaults"] = {},
-	-- 		["core.concealer"] = {
-	-- 			config = {
-	-- 				icon_preset = "basic", --basic, diamond, varied
-	-- 			},
-	-- 		},
-	-- 		["core.dirman"] = {
-	-- 			config = {
-	-- 				workspaces = {
-	-- 					notes = "~/Notes",
-	-- 				},
-	-- 				default_workspace = "notes",
-	-- 			},
-	-- 		},
-	-- 	},
-	-- }),
+  "nvim-neorg/neorg",
+  lazy = false,
+  version = "*",
+  config = true,
+  -- require("neorg").setup({
+  -- 	load = {
+  -- 		["core.defaults"] = {},
+  -- 		["core.concealer"] = {
+  -- 			config = {
+  -- 				icon_preset = "basic", --basic, diamond, varied
+  -- 			},
+  -- 		},
+  -- 		["core.dirman"] = {
+  -- 			config = {
+  -- 				workspaces = {
+  -- 					notes = "~/Notes",
+  -- 				},
+  -- 				default_workspace = "notes",
+  -- 			},
+  -- 		},
+  -- 	},
+  -- }),
 }
