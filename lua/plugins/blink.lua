d4b6138cdbbdb495ee5a3740b4d32bf84e70e748
vaskark <vaskark@gmail.com>
2024-12-20 15:05:35 -0500

    Updates

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,9 +1,6 @@
 return {
   "saghen/blink.cmp",
-  dependencies = {
-    { "rafamadriz/friendly-snippets" },
-    { "L3MON4D3/LuaSnip", version = "v2.*" },
-  },
+  dependencies = { "rafamadriz/friendly-snippets" },
   version = "v0.*",
   opts = {
     keymap = { preset = "default" },
@@ -12,22 +9,5 @@ return {
       nerd_font_variant = "mono",
     },
     signature = { enabled = true },
-    snippets = {
-      expand = function(snippet)
-        require("luasnip").lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require("luasnip").jumpable(filter.direction)
-        end
-        return require("luasnip").in_snippet()
-      end,
-      jump = function(direction)
-        require("luasnip").jump(direction)
-      end,
-    },
-    sources = {
-      default = { "lsp", "path", "luasnip", "buffer" },
-    },
   },
 }
