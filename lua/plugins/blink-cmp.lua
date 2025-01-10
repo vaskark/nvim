c640a886899c9139ad5732b7a6e9933d6f695969
vaskark <vaskark@gmail.com>
2025-01-10 12:32:43 -0500

    Updates

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -11,15 +11,12 @@ return {
       preset = "default",
       ["<Tab>"] = { "snippet_backward", "fallback" },
       ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      
       ["<Up>"] = { "select_next", "fallback" },
       ["<Down>"] = { "select_next", "fallback" },
       ["<C-p>"] = { "select_prev", "fallback" },
       ["<C-n>"] = { "select_next", "fallback" },
-      
       -- ["<CR>"] = { "accept", "fallback" },
       ["<Esc>"] = { "hide", "fallback" },
-      
       ["<S-k>"] = { "scroll_documentation_up", "fallback" },
       ["<S-j>"] = { "scroll_documentation_down", "fallback" },
     },
