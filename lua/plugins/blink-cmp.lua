0637b461f18cdc5d7eb530d23da38545f6f1cf61
vaskark <vaskark@gmail.com>
2025-01-11 12:04:48 -0500

    Updates

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -2,7 +2,6 @@ return {
   "saghen/blink.cmp",
   dependencies = {
     "rafamadriz/friendly-snippets",
-    { "L3MON4D3/LuaSnip", version = "v2.*" },
   },
   version = "*",
   ---@module 'blink.cmp'
@@ -53,9 +52,6 @@ return {
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
-    },
-    snippets = {
-      preset = "luasnip",
     },
     sources = {
       default = {
