0475a0a8da1da666ffe9f0fdeaffd55d62ef7af7
vaskark <vaskark@gmail.com>
2025-01-15 13:28:31 -0500

    Updates

--- lua/plugins/tiny-glimmer.lua
+++ lua/plugins/tiny-glimmer.lua
@@ -2,10 +2,15 @@ return {
   "rachartier/tiny-glimmer.nvim",
   event = "TextYankPost",
   opts = {
-    default_animation = "pulse", -- fade, bounce, left_to_right, pulse, rainbow
-    search = {
-      enabled = true,
-      default_animation = "pulse",
+    default_animation = "pulse", -- fade, reverse_fade, bounce, left_to_right, pulse, rainbow
+    overwrite = {
+      search = {
+        enabled = true,
+        default_animation = "pulse",
+      },
+      paste = {
+        enabled = true,
+      },
     },
     transparency_color = "#000000",
   },
