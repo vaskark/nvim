316b398b1d22e01812edcafd196978854a98c137
vaskark <vaskark@gmail.com>
2025-01-18 05:11:27 -0500

    Updates

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -27,7 +27,7 @@ return {
         { section = "header" },
         -- {
         --   section = "terminal",
-        --   cmd = "ascii-image-converter -C -c ~/Pictures/display/smith.png",
+        --   cmd = "ascii-image-converter -C -c ~/Pictures/display/magneto.png",
         --   height = 30,
         -- },
         { section = "keys", gap = 1, padding = 1 },
@@ -47,24 +47,7 @@ return {
     input = { enabled = true },
     notifier = {
       enabled = true,
-      timeout = 3000,
       style = "compact", -- compact, fancy, minimal
-
-      -- simple LSP progress
-      vim.api.nvim_create_autocmd("LspProgress", {
-        ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-        callback = function(ev)
-          local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-          vim.notify(vim.lsp.status(), "info", {
-            id = "lsp_progress",
-            title = "LSP Progress",
-            opts = function(notif)
-              notif.icon = ev.data.params.value.kind == "end" and " "
-                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-            end,
-          })
-        end,
-      }),
     },
     picker = { enabled = true },
     quickfile = { enabled = true },
