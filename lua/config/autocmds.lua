b71dd50425b054c6ad21c0154d0d2d3943117cf5
vaskark <vaskark@gmail.com>
2025-01-19 10:27:10 -0500

    Updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,7 @@
--- remove bg for lualine (if supported)
+-- Remove bg for lualine (if supported)
 vim.cmd([[autocmd ColorScheme * highlight StatusLine guibg=none]])
 
--- simple LSP progress
+-- Simple LSP progress
 -- vim.api.nvim_create_autocmd("LspProgress", {
 --   ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
 --   callback = function(ev)
@@ -17,7 +17,7 @@ vim.cmd([[autocmd ColorScheme * highlight StatusLine guibg=none]])
 --   end,
 -- })
 
--- advanced LSP progress
+-- Advanced LSP progress
 ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
 local progress = vim.defaulttable()
 vim.api.nvim_create_autocmd("LspProgress", {
@@ -60,4 +60,27 @@ vim.api.nvim_create_autocmd("LspProgress", {
       end,
     })
   end,
+})
+
+-- Restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.cmd('normal! g`"zz')
+    end
+  end,
+})
+
+-- Auto resize splits when the terminal's window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+  command = "wincmd =",
+})
+
+-- Help buffers to the right
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Automatically Split help Buffers to the right",
+  pattern = "help",
+  command = "wincmd L",
 })
