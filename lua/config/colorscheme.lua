5fc8610765a5768a2acb3f758fbe7342efb3bef1
vaskark <vaskark@gmail.com>
2025-02-06 10:53:31 -0500

    Updates

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,14 +1,14 @@
 vim.g.SCHEME = "rosebones"
 
 vim.api.nvim_create_autocmd("VimEnter", {
-	nested = true,
-	callback = function()
-		pcall(vim.cmd.colorscheme, vim.g.SCHEME)
-	end,
+  nested = true,
+  callback = function()
+    pcall(vim.cmd.colorscheme, vim.g.SCHEME)
+  end,
 })
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-	callback = function(params)
-		vim.g.SCHEME = params.match
-	end,
+  callback = function(params)
+    vim.g.SCHEME = params.match
+  end,
 })
--- lua/plugins/jellybeans.lua
+++ lua/plugins/jellybeans.lua
@@ -1,6 +1,6 @@
 return {
   "wtfox/jellybeans.nvim",
-  enabled = true,
+  enabled = false,
   priority = 1000,
   config = function()
     require("jellybeans").setup({
