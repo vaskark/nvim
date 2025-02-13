e3049eaa9daadc65a032181ab881aa24cbf912e0
vaskark <vaskark@gmail.com>
2025-02-13 11:41:19 -0500

    Updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -61,5 +61,5 @@ opt.foldlevel = 99
 opt.formatexpr = "v:lua.require'lazyvim.util'.format.formatexpr()"
 
 -- add binaries installed by mason.nvim to path
--- local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
--- vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath("data") .. "/mason/bin"
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath("data") .. "/mason/bin"
