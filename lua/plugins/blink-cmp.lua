1730ac12e6a58760f7b9f29f6b3a7096bfca2e87
vaskark <vaskark@gmail.com>
2025-02-15 23:30:05 -0500

    Updates

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,84 +1,28 @@
 return {
   "saghen/blink.cmp",
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-  },
+  dependencies = "rafamadriz/friendly-snippets",
   version = "*",
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
     keymap = {
       preset = "default",
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<Tab>"] = { "snippet_forward", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<C-y>"] = { "select_and_accept" },
-      ["<Esc>"] = { "hide", "fallback" },
-      ["<S-k>"] = { "scroll_documentation_up", "fallback" },
-      ["<S-j>"] = { "scroll_documentation_down", "fallback" },
-    },
-    completion = {
-      documentation = {
-        window = {
-          border = "single",
-        },
-      },
-      ghost_text = {
-        enabled = false,
-      },
-      list = {
-        selection = {
-          preselect = function(ctx)
-            return ctx.mode ~= "cmdline"
-          end,
-          auto_insert = function(ctx)
-            return ctx.mode ~= "cmdline"
-          end,
-        },
-      },
-      menu = {
-        -- auto_show = false,
-        auto_show = function(ctx)
-          return ctx.mode ~= "cmdline" or not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype())
-        end,
-        border = "single",
-        draw = {
-          treesitter = { "lsp" },
-        },
-      },
     },
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
     },
-    sources = {
-      default = {
-        "lsp",
-        "path",
-        "snippets",
-        "buffer",
-      },
-      -- cmdline = {},
-      cmdline = function()
-        local type = vim.fn.getcmdtype()
-        -- Search forward and backward
-        if type == "/" or type == "?" then
-          return { "buffer" }
-        end
-        -- Commands
-        if type == ":" then
-          return { "cmdline" }
-        end
-        return {}
-      end,
-    },
-    signature = {
-      enabled = true,
-      window = {
+    completion = {
+      menu = {
+        auto_show = function(ctx)
+          return ctx.mode ~= "cmdline"
+        end,
         border = "single",
       },
+    },
+    signature = { enabled = true },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
     },
   },
   opts_extend = { "sources.default" },
