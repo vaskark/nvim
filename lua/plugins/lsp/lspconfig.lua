e98cfe4edb17fbbc22d21fa5196d277aa2a9057d
vaskark <vaskark@gmail.com>
2025-02-16 12:19:50 -0500

    Updates

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,19 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = {
-    "saghen/blink.cmp",
-    {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          { path = "snacks.nvim", words = { "Snacks" } },
-          { path = "lazy.nvim", words = { "LazyVim" } },
-        },
-      },
-    },
-  },
+  dependencies = { "saghen/blink.cmp" },
   opts = {
     servers = {
       lua_ls = {},
