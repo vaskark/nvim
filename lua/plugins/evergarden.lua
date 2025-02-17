7339fff3cff68f48fe019701214c998817c63147
vaskark <vaskark@gmail.com>
2025-02-17 11:56:40 -0500

    Updates

--- lua/plugins/evergarden.lua
+++ lua/plugins/evergarden.lua
@@ -1,7 +1,7 @@
 return {
   "comfysage/evergarden",
   lazy = true,
-  -- priority = 1000,
+  priority = 1000,
   opts = {
     transparent_background = true,
     variant = "medium", -- 'hard'|'medium'|'soft'
