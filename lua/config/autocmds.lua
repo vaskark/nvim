fd0ffc393bf54f9d12637e87c20632623872012f
vaskark <vaskark@gmail.com>
2025-02-17 18:58:02 -0500

    Updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -51,14 +51,3 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     end
   end,
 })
-
--- markview
-vim.api.nvim_create_autocmd("User", {
-  pattern = "MarkviewAttach",
-  callback = function(event)
-    --- This will have all the data you need.
-    local data = event.data
-
-    vim.print(data)
-  end,
-})
