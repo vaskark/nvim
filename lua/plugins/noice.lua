a7d175afa1354ac153d11f40fe0dcc00378f6f08
vaskark <vaskark@gmail.com>
2025-02-28 21:57:35 -0500

    Updates

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,16 +8,5 @@ return {
     cmdline = {
       view = "cmdline", -- cmdline, cmdline_popup
     },
-    notify = { enabled = false },
-    lsp = {
-      progress = {
-        view = "mini", -- mini, notify
-      },
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-      },
-    },
   },
 }
