05706c2725fcfb8865cda8837afe4d097123d1e9
vaskark <vaskark@gmail.com>
2025-05-20 23:26:19 -0400

    Updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     { import = "plugins" },
-    { import = "plugins.lsp" },
   },
   checker = {
     enabled = true,
@@ -29,3 +28,4 @@ require("lazy").setup({
     notify = false,
   },
 })
+
