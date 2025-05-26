e324060379e7fd1b3e222984f681d63fec7ec626
vaskark <vaskark@gmail.com>
2025-05-26 15:57:18 -0400

    Updates

--- lua/plugins/zenbones.lua
+++ lua/plugins/zenbones.lua
@@ -1,20 +1,20 @@
 return {
   "zenbones-theme/zenbones.nvim",
   dependencies = "rktjmp/lush.nvim",
-  lazy = true,
+  lazy = false,
   priority = 1000,
   config = function()
-    vim.g.duckbones_transparent_background = true
-    vim.g.forestbones_transparent_background = true
-    vim.g.kanagawabones_transparent_background = true
-    vim.g.neobones_transparent_background = true
-    vim.g.nordbones_transparent_background = true
-    vim.g.rosebones_transparent_background = true
-    vim.g.seoulbones_transparent_background = true
-    vim.g.tokyobones_transparent_background = true
-    vim.g.vimbones_transparent_background = true
-    vim.g.zenbones_transparent_background = true
-    vim.g.zenburned_transparent_background = true
-    vim.g.zenwritten_transparent_background = true
+    vim.g.duckbones = { transparent_background = true }
+    vim.g.forestbones = { transparent_background = true }
+    vim.g.kanagawabones = { transparent_background = true }
+    vim.g.neobones = { transparent_background = true }
+    vim.g.nordbones = { transparent_background = true }
+    vim.g.rosebones = { transparent_background = true }
+    vim.g.seoulbones = { transparent_background = true }
+    vim.g.tokyobones = { transparent_background = true }
+    vim.g.vimbones = { transparent_background = true }
+    vim.g.zenbones = { transparent_background = true }
+    vim.g.zenburned = { transparent_background = true }
+    vim.g.zenwritten = { transparent_background = true }
   end,
 }
