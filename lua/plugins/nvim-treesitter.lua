6957c5babc1062031a39c0a170ca846a39c2d494
vaskark <vaskark@gmail.com>
2025-05-28 13:40:00 -0400

    Updates

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,45 +1,33 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	-- event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			auto_install = false,
-			sync_install = false,
-			modules = {},
-			ignore_install = {},
-			highlight = {
-				enable = true,
-				disable = function(lang, buf) -- disable for large files
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			},
-			indent = {
-				enable = true,
-			},
-			autotag = {
-				enable = true,
-			},
-			ensure_installed = {
-				"bash",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"regex",
-				"toml",
-				"vim",
-				"vimdoc",
-				"yaml",
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      autotag = {
+        enable = true,
+      },
+      ensure_installed = {
+        "bash",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+    })
+  end,
 }
