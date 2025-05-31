8cd5a51e2c3766e9611a5af222fcad2ee9a7be88
vaskark <vaskark@gmail.com>
2025-05-31 16:16:31 -0400

    Updates

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,17 +3,6 @@ return {
   dependencies = {
     "rafamadriz/friendly-snippets",
     "moyiz/blink-emoji.nvim",
-    {
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        },
-      },
-    },
   },
   version = "1.*",
   opts = {
