4348a0241af37f955efe37f953dc7ddf17ee1571
vaskark <vaskark@gmail.com>
2025-09-19 22:37:20 -0400

    Updates

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -5,7 +5,6 @@ return {
   config = function()
     vim.g.sonokai_style = "espresso" -- default, atlantis, andromeda, shusia, maia, espresso
     vim.g.sonokai_better_performance = 1
-    vim.g.sonokai_enable_italic = true
     vim.g.sonokai_transparent_background = 2
   end,
 }
