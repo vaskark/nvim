25dc72b54ed43706e02ccbd296d0a065f4b8fe86
vaskark <vaskark@gmail.com>
2025-09-22 18:22:34 -0400

    Updates

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim",
+    "folke/snacks.nvim",
   },
   config = true,
   vim.keymap.set("n", "<leader>ng", "<cmd> Neogit <cr>", { desc = "Neogit" }),
