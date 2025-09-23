12b879367dde72e6b9a229d8e9cac5a7206555be
vaskark <vaskark@gmail.com>
2025-09-23 14:35:44 -0400

    Updates

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -10,13 +10,13 @@ return {
       lua = { "stylua" },
       sh = { "shfmt" },
     },
-    format_on_save = {
-      timeout_ms = 500,
-    },
     formatters = {
       shfmt = {
         append_args = { "-i", "2", "-ci" },
       },
+    },
+    format_on_save = {
+      timeout_ms = 500,
     },
   },
   init = function()
