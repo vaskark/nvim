454a53f2f6f5b754bfec76aae3c1858ccdb52fea
vaskark <vaskark@gmail.com>
2025-09-25 02:03:57 -0400

    Updates

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,6 +3,11 @@ return {
   opts = {},
   vim.keymap.set("n", "<leader>/", function()
     require("Comment.api").toggle.linewise.current()
-  end),
-  vim.keymap.set("v", "<leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>"),
+  end, { desc = "Toggle comment" }),
+  vim.keymap.set(
+    "v",
+    "<leader>/",
+    "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+    { desc = "Toggle comment" }
+  ),
 }
