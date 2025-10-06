2efdd4a0472cb5accf903f910e688aadba879855
vaskark <vaskark@gmail.com>
2025-10-06 19:57:34 -0400

    Updates

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,11 +1,10 @@
-local map = vim.keymap.set
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
   opts = {
     open_mapping = "<C-\\>",
   },
-  map("n", "<leader>th", "<cmd> ToggleTerm direction=horizontal <cr>", { desc = "Horizontal terminal" }),
-  map("n", "<leader>tv", "<cmd> ToggleTerm direction=vertical size=50 <cr>", { desc = "Vertical terminal" }),
-  map("n", "<leader>tf", "<cmd> ToggleTerm direction=float <cr>", { desc = "Floating terminal" }),
+  vim.keymap.set("n", "<leader>th", "<cmd> ToggleTerm direction=horizontal <cr>", { desc = "Horizontal terminal" }),
+  vim.keymap.set("n", "<leader>tv", "<cmd> ToggleTerm direction=vertical size=50 <cr>", { desc = "Vertical terminal" }),
+  vim.keymap.set("n", "<leader>tf", "<cmd> ToggleTerm direction=float <cr>", { desc = "Floating terminal" }),
 }
