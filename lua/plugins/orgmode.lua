a7a3c584d0e9322e79a51b7797b268a4929622a5
vaskark <vaskark@gmail.com>
2025-12-15 03:28:23 -0500

    Updates

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -2,10 +2,9 @@ return {
   "nvim-orgmode/orgmode",
   event = "VeryLazy",
   config = function()
-    -- Setup orgmode
     require("orgmode").setup({
-      org_agenda_files = "~/orgfiles/**/*",
-      org_default_notes_file = "~/orgfiles/refile.org",
+      -- org_agenda_files = "~/orgfiles/**/*",
+      -- org_default_notes_file = "~/orgfiles/refile.org",
     })
   end,
 }
