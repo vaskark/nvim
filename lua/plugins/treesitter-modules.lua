8dcb6ad3142209ec44240f43d21cdb9b8fec797c
vaskark <vaskark@gmail.com>
2026-01-12 14:39:36 -0500

    Updates

--- lua/plugins/treesitter-modules.lua
+++ lua/plugins/treesitter-modules.lua
@@ -1,20 +1,11 @@
 return {
   "MeanderingProgrammer/treesitter-modules.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ---@module 'treesitter-modules'
+  ---@type ts.mod.UserConfig
+  opts = {},
   config = function()
     require("treesitter-modules").setup({
-      indent = { enable = true },
-      highlight = { enable = true },
-      fold = { enable = true },
-      autotag = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        },
-      },
       ensure_installed = {
         "bash",
         "git_config",
@@ -33,6 +24,19 @@ return {
         "vimdoc",
         "yaml",
       },
+      fold = { enable = true },
+      highlight = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+      indent = { enable = true },
+      -- autotag = { enable = true },
     })
   end,
 }
