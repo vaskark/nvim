85a14c8643cd617f845f446a90c40122bcc43cf8
vaskark <vaskark@gmail.com>
2026-03-02 21:03:38 -0500

    Updates

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,10 +1,70 @@
 return {
-  "NvChad/nvim-colorizer.lua",
+  "catgoose/nvim-colorizer.lua",
   event = "BufReadPre",
-  opts = {
-    user_default_options = {
-      names = false,
-      mode = "background", -- background, foreground, virtualtext
-    },
-  },
+  opts = {},
+  config = function()
+    require("colorizer").setup({
+      filetypes = { "*" },
+      buftypes = {},
+      user_commands = true,
+      lazy_load = false,
+      options = {
+        parsers = {
+          css = false, -- preset: enables names, hex, rgb, hsl, oklch
+          css_fn = false, -- preset: enables rgb, hsl, oklch
+          names = {
+            enable = false,
+            lowercase = true,
+            camelcase = true,
+            uppercase = false,
+            strip_digits = false,
+            custom = false, -- table|function|false
+          },
+          hex = {
+            default = false, -- default value for format keys (see above)
+            rgb = false, -- #RGB
+            rgba = false, -- #RGBA
+            rrggbb = false, -- #RRGGBB
+            rrggbbaa = false, -- #RRGGBBAA
+            aarrggbb = false, -- 0xAARRGGBB
+          },
+          rgb = { enable = false },
+          hsl = { enable = false },
+          oklch = { enable = false },
+          tailwind = {
+            enable = false, -- parse Tailwind color names
+            lsp = false, -- use Tailwind LSP documentColor
+            update_names = false,
+          },
+          sass = {
+            enable = false,
+            parsers = { css = true },
+            variable_pattern = "^%$([%w_-]+)",
+          },
+          xterm = { enable = false },
+          custom = {},
+        },
+        display = {
+          mode = "background", -- "background"|"foreground"|"virtualtext"
+          background = {
+            bright_fg = "#000000",
+            dark_fg = "#ffffff",
+          },
+          virtualtext = {
+            char = "■",
+            position = "eol", -- "eol"|"before"|"after"
+            hl_mode = "foreground",
+          },
+          priority = {
+            default = 150, -- vim.hl.priorities.diagnostics
+            lsp = 200, -- vim.hl.priorities.user
+          },
+        },
+        hooks = {
+          should_highlight_line = false, -- function(line, bufnr, line_num) -> bool
+        },
+        always_update = false,
+      },
+    })
+  end,
 }
