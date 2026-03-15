fe5f58381497a2dacd1651d74c3059cfe74d5778
vaskark <vaskark@gmail.com>
2026-03-14 22:51:02 -0400

    Updates

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -66,9 +66,9 @@ return {
         emoji = {
           module = "blink-emoji",
           name = "Emoji",
-          score_offset = 15, -- Tune by preference
+          score_offset = 15,
           opts = {
-            insert = true, -- Insert emoji (default) or complete its name
+            insert = true,
             ---@type string|table|fun():table
             trigger = function()
               return { ":" }
