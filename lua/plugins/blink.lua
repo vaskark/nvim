6bc43ee26cdd25d851134a4dda5f901d4bfe0184
vaskark <vaskark@gmail.com>
2026-03-14 22:34:59 -0400

    Updates

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -35,6 +35,17 @@ return {
         border = "single",
         draw = {
           treesitter = { "lsp" },
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
+          },
         },
       },
       ghost_text = {
