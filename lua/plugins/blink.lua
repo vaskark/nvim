24623b669bc7f4f3737205163938cfab93d3fa06
vaskark <vaskark@gmail.com>
2026-03-14 22:45:50 -0400

    Updates

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -34,22 +34,12 @@ return {
         end,
         border = "single",
         draw = {
-          columns = { { "kind_icon" }, { "label", gap = 1 } },
-          components = {
-            label = {
-              text = function(ctx)
-                return require("colorful-menu").blink_components_text(ctx)
-              end,
-              highlight = function(ctx)
-                return require("colorful-menu").blink_components_highlight(ctx)
-              end,
-            },
-          },
+          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
           treesitter = { "lsp" },
         },
       },
       ghost_text = {
-        enabled = true,
+        enabled = false,
       },
     },
     signature = { enabled = true },
--- lua/plugins/colorful-menu.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "xzbdmw/colorful-menu.nvim",
-}
