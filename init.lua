df73b10c5c17554ee7de44cb7d19d7c3b946b1c3
vaskark <vaskark@gmail.com>
2026-03-15 11:42:24 -0400

    Updates

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
 -- config
-require("config.autocmds")
-require("config.options")
-require("config.keymaps")
-require("config.lazy")
-require("config.lsp")
-require("config.colorscheme")
+require("config")
---
+++ lua/config/init.lua
@@ -0,0 +1,7 @@
+-- config
+require("config.autocmds")
+require("config.options")
+require("config.keymaps")
+require("config.lazy")
+require("config.lsp")
+require("config.colorscheme")
