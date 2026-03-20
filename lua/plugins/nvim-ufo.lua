84c0257cd512b3d831106031de9b0382e5cfe512
vaskark <vaskark@gmail.com>
2026-03-20 10:33:07 -0400

    Updates

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -2,12 +2,6 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" },
   config = function()
-    require("ufo").setup({
-      provider_selector = function(bufnr, filetype, buftype)
-        return { "treesitter", "indent" }
-      end,
-    })
-
     vim.opt.foldcolumn = "1"
     vim.opt.foldlevel = 99
     vim.opt.foldlevelstart = 99
@@ -15,5 +9,11 @@ return {
 
     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+    require("ufo").setup({
+      provider_selector = function(bufnr, filetype, buftype)
+        return { "treesitter", "indent" }
+      end,
+    })
   end,
 }
