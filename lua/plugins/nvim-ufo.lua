1e9e98a086f60746cf323f79aa192d2f3eada6ca
vaskark <vaskark@gmail.com>
2026-03-20 10:30:34 -0400

    Updates

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -7,5 +7,13 @@ return {
         return { "treesitter", "indent" }
       end,
     })
+
+    vim.opt.foldcolumn = "1"
+    vim.opt.foldlevel = 99
+    vim.opt.foldlevelstart = 99
+    vim.opt.foldenable = true
+
+    vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+    vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
   end,
 }
