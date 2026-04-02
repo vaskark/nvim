3c2890219376f77a81f9ac3d95bf8a950fd43d99
vaskark <vaskark@gmail.com>
2026-04-02 00:34:02 -0400

    Updates

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -7,9 +7,9 @@ return {
     "OXY2DEV/markview.nvim",
   },
 
-  init = function()
-    vim.env.CC = "clang"
-  end,
+  -- init = function()
+  --   vim.env.CC = "clang"
+  -- end,
 
   config = function()
     require("nvim-treesitter").install({
@@ -40,6 +40,48 @@ return {
       "yaml",
       "zathurarc",
       "zsh",
+    })
+
+    -- enable
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "bash",
+        "css",
+        "gitcommit",
+        "git_config",
+        "gitignore",
+        "hyprlang",
+        "ini",
+        "json",
+        "kdl",
+        "kitty",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "passwd",
+        "powershell",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "ssh_config",
+        "toml",
+        "vim",
+        "vimdoc",
+        "xresources",
+        "yaml",
+        "zathurarc",
+        "zsh",
+      },
+      callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- folds, provided by Neovim
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo.foldmethod = "expr"
+        -- indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
