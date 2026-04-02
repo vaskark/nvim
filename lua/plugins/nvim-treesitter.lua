022c3905075e5846bae4b027b1b2b238b4fa1053
vaskark <vaskark@gmail.com>
2026-04-01 21:22:46 -0400

    Updates

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,47 +1,45 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    lazy = false,
-    build = ":TSUpdate",
-    dependencies = {
-      "OXY2DEV/markview.nvim",
-    },
-
-    init = function()
-      vim.env.CC = "clang"
-    end,
-
-    config = function()
-      require("nvim-treesitter").install({
-        "bash",
-        "css",
-        "gitcommit",
-        "git_config",
-        "gitignore",
-        "hyprlang",
-        "ini",
-        "json",
-        "kdl",
-        "kitty",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "passwd",
-        "powershell",
-        "python",
-        "query",
-        "regex",
-        "rust",
-        "ssh_config",
-        "toml",
-        "vim",
-        "vimdoc",
-        "xresources",
-        "yaml",
-        "zathurarc",
-        "zsh",
-      })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
+  dependencies = {
+    "OXY2DEV/markview.nvim",
   },
+
+  init = function()
+    vim.env.CC = "clang"
+  end,
+
+  config = function()
+    require("nvim-treesitter").install({
+      "bash",
+      "css",
+      "gitcommit",
+      "git_config",
+      "gitignore",
+      "hyprlang",
+      "ini",
+      "json",
+      "kdl",
+      "kitty",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "passwd",
+      "powershell",
+      "python",
+      "query",
+      "regex",
+      "rust",
+      "ssh_config",
+      "toml",
+      "vim",
+      "vimdoc",
+      "xresources",
+      "yaml",
+      "zathurarc",
+      "zsh",
+    })
+  end,
 }
