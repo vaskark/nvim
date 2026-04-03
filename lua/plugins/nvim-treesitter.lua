b55832a093ce5992fdfef54e6e99ceb02ce77d62
vaskark <vaskark@gmail.com>
2026-04-03 18:31:39 -0400

    Updates

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -41,50 +41,58 @@ return {
       "zsh",
     })
 
-    -- enable
+    -- vim.api.nvim_create_autocmd("FileType", {
+    --   pattern = {
+    --     "bash",
+    --     "css",
+    --     "gitcommit",
+    --     "git_config",
+    --     "gitignore",
+    --     "hyprlang",
+    --     "ini",
+    --     "json",
+    --     "kdl",
+    --     "kitty",
+    --     "lua",
+    --     "markdown",
+    --     "markdown_inline",
+    --     "passwd",
+    --     "powershell",
+    --     "python",
+    --     "query",
+    --     "regex",
+    --     "rust",
+    --     "ssh_config",
+    --     "toml",
+    --     "vim",
+    --     "vimdoc",
+    --     "xresources",
+    --     "yaml",
+    --     "zathurarc",
+    --     "zsh",
+    --   },
+    --   callback = function()
+    --     vim.treesitter.start()
+    --     vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    --     vim.wo.foldmethod = "expr"
+    --     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    --   end,
+    -- })
+
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = {
-        "bash",
-        "css",
-        "gitcommit",
-        "git_config",
-        "gitignore",
-        "hyprlang",
-        "ini",
-        "json",
-        "kdl",
-        "kitty",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "passwd",
-        "powershell",
-        "python",
-        "query",
-        "regex",
-        "rust",
-        "ssh_config",
-        "toml",
-        "vim",
-        "vimdoc",
-        "xresources",
-        "yaml",
-        "zathurarc",
-        "zsh",
-      },
       callback = function()
-        vim.treesitter.start()
-        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        vim.wo.foldmethod = "expr"
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        if pcall(vim.treesitter.start) then
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.wo.foldmethod = "expr"
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
       end,
     })
 
-    -- run TSUpdate whenever nvim-treesitter updates
     vim.api.nvim_create_autocmd("PackChanged", {
       callback = function(ev)
         local name, kind = ev.data.spec.name, ev.data.kind
-        if name == "nvim-treesitter" and kind == "update" then
+        if name == "nvim-treesitter" and (kind == "install" or kind == "update") then
           if not ev.data.active then
             vim.cmd.packadd("nvim-treesitter")
           end
