a8c38ce6b44844786f3b92f8743bbaee0c4e485b
vaskark <vaskark@gmail.com>
2026-04-12 12:21:44 -0400

    Updates

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -24,62 +24,18 @@ return {
       "json",
       "kdl",
       "kitty",
-      "lua",
-      "markdown",
-      "markdown_inline",
       "passwd",
       "powershell",
       "python",
-      "query",
       "regex",
       "rust",
       "ssh_config",
       "toml",
-      "vim",
-      "vimdoc",
       "xresources",
       "yaml",
       "zathurarc",
       "zsh",
     })
-
-    -- vim.api.nvim_create_autocmd("FileType", {
-    --   pattern = {
-    --     "bash",
-    --     "css",
-    --     "gitcommit",
-    --     "git_config",
-    --     "gitignore",
-    --     "hyprlang",
-    --     "ini",
-    --     "json",
-    --     "kdl",
-    --     "kitty",
-    --     "lua",
-    --     "markdown",
-    --     "markdown_inline",
-    --     "passwd",
-    --     "powershell",
-    --     "python",
-    --     "query",
-    --     "regex",
-    --     "rust",
-    --     "ssh_config",
-    --     "toml",
-    --     "vim",
-    --     "vimdoc",
-    --     "xresources",
-    --     "yaml",
-    --     "zathurarc",
-    --     "zsh",
-    --   },
-    --   callback = function()
-    --     vim.treesitter.start()
-    --     vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    --     vim.wo.foldmethod = "expr"
-    --     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    --   end,
-    -- })
 
     vim.api.nvim_create_autocmd("FileType", {
       callback = function()
