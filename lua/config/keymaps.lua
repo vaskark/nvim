8b4fedab0ae34c25a5438363d6deb2209d775f92
vaskark <vaskark@gmail.com>
2026-04-12 02:09:35 -0400

    Updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,9 +3,6 @@ local map = vim.keymap.set
 -- save
 map({ "n", "i" }, "<C-s>", "<cmd> w <cr>", { desc = "Save File" })
 
--- reload
-map("n", "<leader><leader>r", "<cmd> update <cr> <cmd> source <cr>", { desc = "Reload File" })
-
 -- exit insert mode
 map("i", "jj", "<esc>", { desc = "Exit Insert Mode" })
 
@@ -24,3 +21,10 @@ map("n", "<C-h>", "<C-w>h", { desc = "Move Focus to Left Window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move Focus to Lower Window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move Focus to Upper Window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move Focus to Right Window" })
+
+-- restart
+vim.keymap.set("n", "<leader>R", function()
+  local session = vim.fn.stdpath("state") .. "/restart_session.vim"
+  vim.cmd("mksession! " .. vim.fn.fnameescape(session))
+  vim.cmd("restart source " .. vim.fn.fnameescape(session))
+end, { desc = "Restart neovim" })
---
+++ lua/plugins/ember.lua
@@ -0,0 +1,6 @@
+return {
+  "ember-theme/nvim",
+  name = "ember",
+  lazy = true,
+  priority = 1000,
+}
