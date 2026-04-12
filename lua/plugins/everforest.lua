b9b5a57b3b777750c1786135c58d15cfda46191e
vaskark <vaskark@gmail.com>
2026-04-12 12:51:46 -0400

    Updates

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,10 +1,11 @@
 return {
-  "sainnhe/everforest",
+  "neanias/everforest-nvim",
   lazy = true,
   priority = 1000,
   config = function()
-    vim.g.everforest_background = "medium" -- hard, medium (default), soft
-    vim.g.everforest_better_performance = 1
-    vim.g.everforest_transparent_background = 2
+    require("everforest").setup({
+      transparent_background_level = 2,
+      italics = true,
+    })
   end,
 }
